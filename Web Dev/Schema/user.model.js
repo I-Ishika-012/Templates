25e5c5a88ce30cde4eb0d2c10e83c8c9ef92c0d1
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-07-31 23:56:22 +0530

    Add files via upload

---
+++ Web Dev/Schema/user.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: Number, required: true, unique: true },
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema)
