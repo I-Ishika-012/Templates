3df7c77f9431387f8f3126780481fe9dccd08335
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-08-01 10:15:13 +0530

    Update and rename user.controller.js to login.controller.js

---
+++ Web Dev/Controllers/login.controller.js
@@ -0,0 +1,60 @@
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User does not exist", success: false });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect password", success: false });
+    }
+    //role
+    if (user.role !== role) {
+      return res
+        .status(400)
+        .json({
+          message: "Account does not belong to this role",
+          success: false,
+        });
+    }
+    //!generate token
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      role: user.role,
+      phoneNumber: user.phoneNumber,
+      profile: user.profile,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({ message: `Welcome ${user.fullname}`, success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
